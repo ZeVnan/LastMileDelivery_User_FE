0cc264aba242e721ed053347fcabd313810407dc
ZeVnan <21521812@gm.uit.edu.vn>
2024-12-18 17:37:20 +0700

    push notification #2: remove firebase messaging

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as Notification from 'expo-notifications';
-import * as Device from 'expo-device';
-import firebaseApp from './Utilities/FirebaseConfig';
 
 import HomeScreen from './Screens/HomeScreen';
 import ChatScreen from './Screens/ChatScreen';
@@ -23,30 +20,6 @@ import { UserProvider } from './Utilities/UserContext';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  async function registerForPushNotification() {
-    if (Device.isDevice){
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      const token = (await Notifications.getExpoPushTokenAsync()).data;
-      //console.log("Expo Push Token:", token);
-    }
-    else{
-      alert("Must be physical device for push notification");
-    }
-  }
-  useEffect(() => {
-    registerForPushNotification();
-  }, []);
-
   return (
     <UserProvider>
       <SafeAreaView style={styles.container}>
